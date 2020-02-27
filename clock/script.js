f02155fdab8f263a363f0734e286a99c5b617220
jjennyjjordan <47002344+jjennyjjordan@users.noreply.github.com>
2020-02-27 15:11:33 -0500

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $(".M").addClass("alarm");
+  }, 3000);
+  $(".button1").addClass("alarm");
+  }, 3000);
+  $(".button2").addClass("alarm");
+  }, 3000);
+  $(".H").addClass("alarm");
+  }, 3000);
+
+});
